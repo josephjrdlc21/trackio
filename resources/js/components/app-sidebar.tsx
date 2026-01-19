23f8466db901d19fe7fc2591ebc77444a35874e2
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2026-01-19 12:00:52 +0800

    feat: add UI components for skeleton, sonner, switch, table, textarea, and tooltip
    - Implemented Skeleton component for loading states.
    - Added Sonner component for notifications with customizable icons and themes.
    - Created Switch component using Radix UI for toggle functionality.
    - Developed Table component with subcomponents for structured data display.
    - Introduced Textarea component for multi-line text input.
    - Refactored Tooltip component with improved structure and styling.
    
    chore: update tsconfig to ignore deprecations

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,100 +1,90 @@
 import * as React from "react"
 import { Coins, LayoutDashboard, HandCoins, WalletCards } from "lucide-react"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-} from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarHeader, SidebarMenu,
+  	SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton,
+  	SidebarMenuSubItem,} from "@/components/ui/sidebar"
 
-// This is sample data.
 const data = {
-  navMain: [
-    {
-      title: "Home",
-      url: "#",
-      items: [
-        {
-          title: "Dashboard",
-          url: "#",
-          isActive: "",
-          icon: <LayoutDashboard className="size-4" />,
-        },
-      ],
-    },
-    {
-      title: "Money Management",
-      url: "#",
-      items: [
-        {
-          title: "Expenses",
-          url: "#",
-          icon: <HandCoins className="size-4" />,
-        },
-        {
-          title: "Income",
-          url: "#",
-          icon: <WalletCards className="size-4" />,
-        },
-      ],
-    },
-  ],
+	navMain: [
+		{
+		title: "Home",
+			url: "#",
+			items: [
+				{
+					title: "Dashboard",
+					url: "#",
+					isActive: "",
+					icon: <LayoutDashboard className="size-4" />,
+				},
+			],
+		},
+		{
+		title: "Money Management",
+		url: "#",
+			items: [
+				{
+					title: "Expenses",
+					url: "#",
+					icon: <HandCoins className="size-4" />,
+				},
+				{
+					title: "Income",
+					url: "#",
+					icon: <WalletCards className="size-4" />,
+				},
+			],
+		},
+	],
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar variant="sidebar" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" className="hover:bg-transparent" asChild>
-              <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Coins className="size-5 text-white" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold text-lg">Trackio</span>
-                </div>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenu className="gap-2">
-            {data.navMain.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton className="hover:bg-transparent" asChild>
-                  <a href={item.url} className="font-semibold">
-                    {item.title}
-                  </a>
-                </SidebarMenuButton>
-                {item.items?.length ? (
-                  <SidebarMenuSub className="ml-0 border-l-0 px-1.5 mx-0">
-                    {item.items.map((item) => (
-                      <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton className="py-5" asChild>
-                          <a href={item.url} className="flex items-center gap-4">
-                            {item.icon}
+	return (
+		<Sidebar variant="sidebar" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" className="hover:bg-transparent" asChild>
+							<a href="#">
+								<div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+									<Coins className="size-5 text-white" />
+								</div>
+								<div className="flex flex-col gap-0.5 leading-none">
+									<span className="font-semibold text-lg">Trackio</span>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarMenu className="gap-2">
+						{data.navMain.map((item) => (
+							<SidebarMenuItem key={item.title}>
+								<SidebarMenuButton className="hover:bg-transparent" asChild>
+									<a href={item.url} className="font-semibold">
+										{item.title}
+									</a>
+								</SidebarMenuButton>
+								{item.items?.length ? (
+									<SidebarMenuSub className="ml-0 border-l-0 px-1.5 mx-0">
+										{item.items.map((item) => (
+											<SidebarMenuSubItem key={item.title}>
+												<SidebarMenuSubButton className="py-5" asChild>
+													<a href={item.url} className="flex items-center gap-4">
+														{item.icon}
 														<span>{item.title}</span>
-                          </a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                ) : null}
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  )
+													</a>
+												</SidebarMenuSubButton>
+											</SidebarMenuSubItem>
+										))}
+									</SidebarMenuSub>
+								) : null}
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	)
 }
